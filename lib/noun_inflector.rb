e119587a608d4bf1236648f3c2d337e7c8df9086
Ron Hopper <rchopper@gmail.com>
2011-06-26 19:41:46 -0400

    Added second declension accented/unaccented -os words to noun inflector.

--- lib/noun_inflector.rb
+++ lib/noun_inflector.rb
@@ -2,17 +2,47 @@
 module NounInflector
 
   def self.inflect(lemma, inflection)
-    lemma[0..-2] + ENDINGS[inflection]
+    if lemma[-1] == 'ή'
+      lemma[0..-2] + D1_ENDINGS[inflection]
+    elsif lemma[-2..-1] == 'ός'
+      lemma[0..-3] + D2_ACCENTED_ENDINGS[inflection]
+    elsif lemma[-2..-1] == 'ος'
+      stem = lemma[0..-3]
+      stem = move_oxia_to_end(stem) if [:gs, :ds, :gp, :dp, :ap].include?(inflection)
+      stem + D2_UNACCENTED_ENDINGS[inflection]
+    end
   end
 
 private
 
-  ENDINGS = {
+  def self.move_oxia_to_end(stem)
+    new_stem = stem.tr('άἄἅᾴέἔἕήἤἥῄίἴἵόὄὅύὔὕώὤὥῴ', 'αἀἁᾳεἐἑηἠἡῃιἰἱοὀὁυὐὑωὠὡῳ')
+    i = new_stem.rindex(/[αἀἁᾳεἐἑηἠἡῃιἰἱοὀὁυὐὑωὠὡῳ]/)
+    new_stem[i] = stem[i].tr('αἀἁᾳεἐἑηἠἡῃιἰἱοὀὁυὐὑωὠὡῳ', 'άἄἅᾴέἔἕήἤἥῄίἴἵόὄὅύὔὕώὤὥῴ')
+    new_stem
+  end
+
+  D1_ENDINGS = {
     :ns => 'ή',  :np => 'αί',
     :gs => 'ῆς', :gp => 'ῶν',
     :ds => 'ῇ',  :dp => 'αῖς',
     :as => 'ήν', :ap => 'άς',
     :vs => 'ή',  :vp => 'αί'
+  }
+
+  D2_UNACCENTED_ENDINGS = {
+    :ns => 'ος', :np => 'οι',
+    :gs => 'ου', :gp => 'ων',
+    :ds => 'ῳ',  :dp => 'οις',
+    :as => 'ον', :ap => 'ους',
+    :vs => 'ε',  :vp => 'οι'
+  }
+  D2_ACCENTED_ENDINGS = {
+    :ns => 'ός', :np => 'οί',
+    :gs => 'οῦ', :gp => 'ῶν',
+    :ds => 'ῷ',  :dp => 'οῖς',
+    :as => 'όν', :ap => 'ούς',
+    :vs => 'έ',  :vp => 'οί'
   }
 
 end
--- spec/noun_inflector_spec.rb
+++ spec/noun_inflector_spec.rb
@@ -16,5 +16,55 @@ describe NounInflector do
     specify { subject.inflect('ἀρχή', :vp).should == 'ἀρχαί' }
   end
 
+  describe "second-declension nouns (early-accent)" do
+    specify { subject.inflect('ἄνθρωπος', :ns).should == 'ἄνθρωπος' }
+    specify { subject.inflect('ἄνθρωπος', :gs).should == 'ἀνθρώπου' }
+    specify { subject.inflect('ἄνθρωπος', :ds).should == 'ἀνθρώπῳ' }
+    specify { subject.inflect('ἄνθρωπος', :as).should == 'ἄνθρωπον' }
+    specify { subject.inflect('ἄνθρωπος', :vs).should == 'ἄνθρωπε' }
+    specify { subject.inflect('ἄνθρωπος', :np).should == 'ἄνθρωποι' }
+    specify { subject.inflect('ἄνθρωπος', :gp).should == 'ἀνθρώπων' }
+    specify { subject.inflect('ἄνθρωπος', :dp).should == 'ἀνθρώποις' }
+    specify { subject.inflect('ἄνθρωπος', :ap).should == 'ἀνθρώπους' }
+    specify { subject.inflect('ἄνθρωπος', :vp).should == 'ἄνθρωποι' }
+  end
+
+  describe "second-declension nouns (final-accent)" do
+    specify { subject.inflect('ἀδελφός', :ns).should == 'ἀδελφός' }
+    specify { subject.inflect('ἀδελφός', :gs).should == 'ἀδελφοῦ' }
+    specify { subject.inflect('ἀδελφός', :ds).should == 'ἀδελφῷ' }
+    specify { subject.inflect('ἀδελφός', :as).should == 'ἀδελφόν' }
+    specify { subject.inflect('ἀδελφός', :vs).should == 'ἀδελφέ' }
+    specify { subject.inflect('ἀδελφός', :np).should == 'ἀδελφοί' }
+    specify { subject.inflect('ἀδελφός', :gp).should == 'ἀδελφῶν' }
+    specify { subject.inflect('ἀδελφός', :dp).should == 'ἀδελφοῖς' }
+    specify { subject.inflect('ἀδελφός', :ap).should == 'ἀδελφούς' }
+    specify { subject.inflect('ἀδελφός', :vp).should == 'ἀδελφοί' }
+  end
+
+=begin
+  describe "first-declension eta nouns with penultimate accent" do
+  LETTERS = "αβγδεζηθικλμνξοπρςστυφχψω"
+  ALPHA   = "αὰάᾶἀἂἄἆἁἃἅἇᾳᾲᾴᾷ"
+  EPSILON = "εὲέ ἐἒἔ ἑἓἕ     "
+  ETA     = "ηὴήῆἠἢἤἦἡἣἥἧῃῂῄῇ"
+  IOTA    = "ιὶίῖἰἲἴἶἱἳἵἷ    "
+  OMICRON = "οὸό ὀὂὄ ὁὃὅ     "
+  RHO     = "ρ   ῤ   ῥ       "
+  UPSILON = "υὺύῦὐὒὔὖὑὓὕὗ    "
+  OMEGA   = "ωὼώῶὠὢὤὦὡὣὥὧῳῲῴῷ"
+    specify { subject.inflect('ἀγάπη', :ns).should == 'ἀγάπη' }
+    specify { subject.inflect('ἀγάπη', :gs).should == 'ἀγάπης' }
+    specify { subject.inflect('ἀγάπη', :ds).should == 'ἀγάπῃ' }
+    specify { subject.inflect('ἀγάπη', :as).should == 'ἀγάπην' }
+    specify { subject.inflect('ἀγάπη', :vs).should == 'ἀγάπη' }
+    specify { subject.inflect('ἀγάπη', :np).should == 'ἀγάπαι' }
+    specify { subject.inflect('ἀγάπη', :gp).should == 'ἀγάπῶν' }
+    specify { subject.inflect('ἀγάπη', :dp).should == 'ἀγάπαις' }
+    specify { subject.inflect('ἀγάπη', :ap).should == 'ἀγάπας' }
+    specify { subject.inflect('ἀγάπη', :vp).should == 'ἀγάπαι' }
+  end
+=end
+
 end
 
